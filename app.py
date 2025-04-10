592448ddc15980b1fdce773f5ead7143f304e334
maxmendiguchiameuser <maxmendiguchiameuser@gmail.com>
2025-04-10 13:45:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -182,7 +182,6 @@ colorbar_html = f"""
 ">
     <div>{vmax_scaled:.2f}</div>
     <div style="
-        
         background: linear-gradient(to top, #9e0142, #d53e4f, #f46d43, #fdae61, #fee08b, #e6f598, #abdda4, #66c2a5, #3288bd, #5e4fa2); */
         width: 10px;
         height: 260px;
